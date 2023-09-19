f49d0c89c86b32cb5c43fd582ddb41e2a967d3f4
Debatreyta <debatreyadas@gmail.com>
2023-09-19 15:31:32 +0530

    config

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');//import express
 const { listen } = require('express/lib/application');
-
+const serverConfig = require('./configs/server.config')
 
 const app = express();//central character of server
 
 
-app.listen(3333, () => {//localhost:3333
+app.listen(serverConfig.PORT, () => {//localhost:PORT and changable things are stored in the configs folders
     console.log('server started');
 })
